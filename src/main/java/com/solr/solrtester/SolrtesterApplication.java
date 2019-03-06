b8b08f59f88f0987daa33e801641586fdad47745
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-03-06 08:47:28 +0800

    创建项目

---
+++ src/main/java/com/solr/solrtester/SolrtesterApplication.java
@@ -0,0 +1,13 @@
+package com.solr.solrtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrtesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrtesterApplication.class, args);
+    }
+
+}
